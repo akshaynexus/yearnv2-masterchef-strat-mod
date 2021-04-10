0fb0f7c45ffe99ac3d9e79168d4dafcd9b3be08d
akshaynexus <akshaycm@hotmail.com>
2021-04-11 00:22:48 +0300

    feat: add deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from brownie import Strategy, accounts, config, network, project, web3
+from brownie import Strategy, accounts, config, network, project, web3, Contract
 from eth_utils import is_checksum_address
 import click
 
@@ -10,24 +10,25 @@ Vault = project.load(
 ).Vault
 
 
-def get_address(msg: str, default: str = None) -> str:
-    val = click.prompt(msg, default=default)
-
-    # Keep asking user for click.prompt until it passes
+def get_address(msg: str) -> str:
     while True:
-
+        val = input(msg)
         if is_checksum_address(val):
             return val
-        elif addr := web3.ens.address(val):
-            click.echo(f"Found ENS '{val}' [{addr}]")
-            return addr
+        else:
+            addr = web3.ens.address(val)
+            if addr:
+                print(f"Found ENS '{val}' [{addr}]")
+                return addr
+        print(f"I'm sorry, but '{val}' is not a checksummed address or ENS")
 
-        click.echo(
-            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
-        )
-        # NOTE: Only display default once
-        val = click.prompt(msg)
 
+def get_pid(want, chef):
+    for i in range(chef.poolLength()):
+        if chef.poolInfo(i)[0] == want:
+            return i
+    #if we found no pid for want,raise valueError
+    raise ValueError("No pid found for Want")
 
 def main():
     print(f"You are using the '{network.show_active()}' network")
@@ -54,5 +55,10 @@ def main():
     publish_source = click.confirm("Verify source on etherscan?")
     if input("Deploy Strategy? y/[N]: ").lower() != "y":
         return
+    masterchef = "0xd7fa57069E4767ddE13aD7970A562c43f03f8365"
+    reward = "0xf33121A2209609cAdc7349AcC9c40E41CE21c730"
+    router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
 
-    strategy = Strategy.deploy(vault, {"from": dev}, publish_source=publish_source)
+    pid = get_pid(vault.token(),Contract.from_explorer(masterchef))
+    print(pid)
+    strategy = Strategy.deploy(vault, masterchef, reward,router,pid,{"from": dev}, publish_source=publish_source)
